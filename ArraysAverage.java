0ac992f4c610ccc425ad32acb0fe58e6a21c683e
Tuncay Demir <tuncaydemir682@gmail.com>
2022-04-26 15:34:48 +0300

    Create ArraysAverage.java

---
+++ ArraysAverage.java
@@ -0,0 +1,37 @@
+public class ArraysAverage{
+    public static void main(String[] args) {
+
+        int[] list = {55, 25, 40, 65, 70, 35};
+
+        double sum = 0;
+
+        double sum1 = 0.0;
+
+
+        for(int i = 0; i < list.length; i++){
+
+            sum += list[i];
+
+
+
+        }
+
+        System.out.println("Average : " + sum / list.length);
+
+        for(int i = 1; i <= list.length; i++){
+
+            sum1 += (1.0 / i);
+
+
+
+        }
+
+        System.out.println("Harmonic Average : " + sum1);
+
+
+
+
+
+
+    }
+}
